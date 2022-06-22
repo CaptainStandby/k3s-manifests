30e9079f4eed09b1d2a1c646777ad6149e14377c
CaptainStandby <18215579+CaptainStandby@users.noreply.github.com>
2022-06-22 11:29:08 +0200

    nex try to use env in config

---
+++ manifests/openhab/openhab/config/init_config.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+set -e
+
+SRC_DIR="${1:?'Please provide a source directory'}"
+DST_DIR="${2:?'Please provide a destination directory'}"
+
+function get_ext {
+	declare -r file="${1:?'Please provide a file'}"
+
+	[[ "$file" = *.* ]] && echo ".${file##*.}" || echo ''
+}
+
+find "$SRC_DIR/" -type f -print0 | while IFS= read -r -d $'\0' file; do
+	file_name="$(basename "$file")"
+	ext="$(get_ext "$file_name")"
+	dir="$(dirname "$file")/"
+	dst_dir="$DST_DIR/${dir#"$SRC_DIR/"}"
+
+	mkdir -p "$dst_dir"
+
+	if [ "$ext" = ".template" ]; then
+		file_no_ext="${file_name%.*}"
+		envsubst < "$file" > "$dst_dir/$file_no_ext"
+	else
+		cp "$file" "$dst_dir/$file_name"
+	fi
+done
--- manifests/openhab/openhab/config/templates/apply_templates.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-sed "s/\${INFLUXDB_ADMIN_TOKEN}/$(printf '%s\n' "$INFLUXDB_ADMIN_TOKEN" | sed -e 's/[\/&]/\\&/g')/" < /templates/influxdb.cfg.template > "$OPENHAB_CONF/services/influxdb.cfg"
